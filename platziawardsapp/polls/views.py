e97c7d04c636b349396bc31e91045c2658af67b7
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-20 15:08:42 -0500

    fixing detail view for future question

--- platziawardsapp/polls/views.py
+++ platziawardsapp/polls/views.py
@@ -42,6 +42,10 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """Excludes any question that aren't published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
