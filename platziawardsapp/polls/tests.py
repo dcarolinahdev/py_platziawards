0209eeca69efb19ae69c4139b4888795cd82dbe0
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-20 15:10:21 -0500

    Adding test for past question in detail view

--- platziawardsapp/polls/tests.py
+++ platziawardsapp/polls/tests.py
@@ -117,3 +117,13 @@ class QuestionDetailViewTest(TestCase):
         url = reverse("polls:detail", args=(future_question.pk,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question("Past question", days=-30)
+        url = reverse("polls:detail", args=(past_question.pk,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
