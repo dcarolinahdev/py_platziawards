47929615fe44c6dcf840bd5a057968ed0d4527d2
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-20 12:24:11 -0500

    Adding test case for was_published_recently with past and present questions

--- platziawardsapp/polls/tests.py
+++ platziawardsapp/polls/tests.py
@@ -15,3 +15,17 @@ class QuestionModelTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text='Who is the best CD in Platzi?', pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently returns False for questions
+        whose pub_date is in the past."""
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text='Who is the best CD in Platzi?', pub_date=time)
+        self.assertFalse(past_question.was_published_recently())
+
+    def test_was_published_recently_with_present_questions(self):
+        """was_published_recently returns False for questions
+        whose pub_date is in the present."""
+        time = timezone.now()
+        present_question = Question(question_text='Who is the best CD in Platzi?', pub_date=time)
+        self.assertTrue(present_question.was_published_recently())
