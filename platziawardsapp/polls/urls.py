2ccd4d663acf87d43b6109472b92f2b561a75687
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-20 14:03:04 -0500

    Updating polls urls

--- platziawardsapp/polls/urls.py
+++ platziawardsapp/polls/urls.py
@@ -5,7 +5,7 @@ app_name = "polls"
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('poll/<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
+    path('poll/<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    path('poll/<int:question_id>/vote/', views.vote, name='vote'),
 ]
